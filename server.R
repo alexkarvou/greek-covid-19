29f9cd8111b23da491b43868b24fa2145e0524a4
alexkarvou <karvouniaris.alexander@gmail.com>
2020-03-14 16:22:53 +0200

    map component and cleaning of athena

--- server.R
+++ server.R
@@ -15,54 +15,12 @@ function(input, output, session) {
       vars$local_image_index<-vars$local_image_index +1}
   })
 
-  observeEvent(input$previousButton,{
-    if (vars$local_image_index>1){
-      vars$local_image_index<-vars$local_image_index -1}
-  })
 
-  observeEvent(input$loadButton,{
-    vars$local_folder_length<-length(list.files(input$LoadFileInput))
-  })
-
-  local_image_path<-eventReactive(c(input$loadButton,input$nextButton,input$previousButton),{
-    paste(input$LoadFileInput,"/",list.files(input$LoadFileInput)[vars$local_image_index],sep="")
-  })
-
-
-
-
-  observeEvent(c(input$loadButton,input$nextButton,input$previousButton),{
-    output$ImageRendering<-renderImage({
-
-
-      filename<-local_image_path()
-
-      width  <- session$clientData$output_ImageRendering_width
-      height <- session$clientData$output_ImageRendering_height
-
-      list(src = filename,
-           width=width,height=height,
-           alt = paste("Couldnt load image"))
-
-    },deleteFile = FALSE)})
-
-
-  observeEvent(input$loadButton,{
-    output$image_Name<-renderText({
-      paste(list.files(input$LoadFileInput)[vars$local_image_index],", #",as.character(vars$local_image_index)," of ",
-            as.character(vars$local_folder_length)," Images.")
-      })
-  })
-
-  observeEvent(input$plot_brush,{
-    vars$current_list<-c(input$plot_brush$xmin,input$plot_brush$ymin,input$plot_brush$xmax,input$plot_brush$ymax)
-  })
-
-  output$box_info<-renderText({paste(names(input$plot_brush))})
-
-  #billboard outputs
-  output$billboard <- renderTimevis({
-    timevis(data)
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+    addTiles() %>%  # use the default base map which is OpenStreetMap tiles
+    addMarkers(lng=23.7275, lat=37.9838,
+               popup="Greek Covid-19 incidents")
   })
 
 
