c2b5c116f1e12217df61f8bb34c6fa049b333161
alexkarvou <karvouniaris.alexander@gmail.com>
2020-03-15 23:15:20 +0200

    db first entries

--- ui.R
+++ ui.R
@@ -6,12 +6,13 @@ library(leaflet)
 
 
 dashboardPage(skin='blue',
-                    dashboardHeader(title = h3(tags$b('Greek Covid-19'))),
+                    dashboardHeader(title = h3(tags$b('Greece - Covid-19'))),
                     dashboardSidebar(
                       sidebarMenu(
-                        menuItem("Dashboard", tabName = "dashboard", icon = icon("home")),
-                        menuItem("News", tabName = "news", icon = icon("rss")),
-                        menuItem("User entry", tabName = "measures", icon = icon("file-medical"))
+                        menuItem("Αρχική", tabName = "dashboard", icon = icon("home")),
+                        menuItem("Εισαγωγή συμπτωμάτων", tabName = "entries", icon = icon("clipboard")),
+                        menuItem("Νέα", tabName = "news", icon = icon("rss"))
+
                       )),
                     dashboardBody(
                       tabItems(
@@ -20,9 +21,17 @@ dashboardPage(skin='blue',
                               column(width=12,
                                      box(width=NULL,solidHeader = TRUE,
                                        leafletOutput("mymap")),
-                                     infoBox("Επιβεβαιωμένα", 10 * 2, icon = icon("user-md"),color="purple"),
-                                     infoBox("Ενεργα", 10 * 2, icon = icon("ambulance"),color='navy'),
-                                     infoBox("Ξεπεράστηκαν", 10 * 2, icon = icon("heartbeat"),color='olive'),
-                                     infoBox("Νεκροί", 10 * 2 ),color='red'))),
-                        tabItem(tabName='news',h2('Nothing Yet'))
-                      )))
+                                     box(
+                                     infoBox("Επιβεβαιωμένα", 0, icon = icon("user-md"),color="purple"),
+                                     infoBox("Ενεργα", 0, icon = icon("ambulance"),color='navy'),
+                                     infoBox("Ξεπεράστηκαν",0, icon = icon("heartbeat"),color='olive'),
+                                     infoBox("Νεκροί", 0 ,color='red'))))),
+                        tabItem(tabName='entries',
+                        fluidRow(
+                              column(width=12,
+                                     box(width=NULL,solidHeader = TRUE,
+                                       selectInput("choice_1",'Τον έχεις:',
+                                          choices=c('Ναι','Οχι')),
+                                       actionButton('push_entry','Καταχώρηση',
+                                           icon=icon('database')))))),
+                        tabItem(tabName='news',h2('Nothing Yet')))))
